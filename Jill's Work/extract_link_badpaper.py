1f49c8c828ab44fcc62f9370b6dfe90f713cbee5
moonish1211 <jillnomura1211@gmail.com>
2024-10-31 15:38:06 -0700

    Added a second bad paper
    Attempted to scrape the second irrelevent paper, but 403 error.

--- Jill's Work/extract_link_badpaper.py
+++ Jill's Work/extract_link_badpaper.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 #Fetching the webpage content using url
-url = 'https://www.nature.com/articles/s41467-019-08507-4'
-response = requests.get(url)
+url_1 = 'https://www.nature.com/articles/s41467-019-08507-4'
+response_1 = requests.get(url_1)
 
 # Check if the page is fetched successfully
-if response.status_code == 200:
+if response_1.status_code == 200:
     # Parse the page content using Beautiful Soup
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response_1.content, 'html.parser')
 
     # Find the class where all the reference is stored as a list
     references = soup.find_all('li', class_='c-article-references__item')
@@ -22,6 +22,41 @@ if response.status_code == 200:
             # print(link['href'])
             link_list.append(link['href'])
     
+
+else:
+    print(f"Failed to fetch the page. Status code: {response_1.status_code}")
+
+url_2 = 'https://www.sciencedirect.com/science/article/pii/S2590238524001711'
+response_2 = requests.get(url_2)
+
+if response_2.status_code == 200:
+    #Parse
+    soup = BeautifulSoup(response_2.content, 'html.parser')
+    references = soup.find_all('li', class_='references')
+    print(references)
+
+    for ref in references:
+        link = ref.find('a', href = True)
+        if link:
+            link_list.append(link['href'])
+
+else:
+    print(f"Failed to fetch second page. Status code: {response_2.status_code}")
+
+print(link_list)
+
+#Store this result in pandas df
+bad_paperdf = pd.DataFrame({
+"link": link_list
+})
+#Create a doi row for just the doi in this link column
+bad_paperdf["doi"] = bad_paperdf["link"].apply(lambda x: x.split('doi.org/')[-1] if 'doi.org/' in x else None)
+
+#Export this pdf as a csv file
+bad_paperdf.to_csv("Irrelevent_paper", index=False)
+
+'''
+DO THIS ONCE link_list is completed!!!
     #Store this result in pandas df
     bad_paperdf = pd.DataFrame({
     "link": link_list
@@ -34,6 +69,4 @@ if response.status_code == 200:
 
     #Export this pdf as a csv file
     bad_paperdf.to_csv("Jill's Work/bad_paper_big.csv", index=False)
-
-else:
-    print(f"Failed to fetch the page. Status code: {response.status_code}")
+    '''
